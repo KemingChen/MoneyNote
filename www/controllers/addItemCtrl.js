740a15fed43a4d3b56149cceb5ca6f72adae302f
KemingChen <believe75467@gmail.com>
2014-03-12 00:09:47 +0800

    完成 AddItem's control 建置 未完成項目:   Class的選擇   檢察值是否無誤   插入DB

--- www/controllers/addItemCtrl.js
+++ www/controllers/addItemCtrl.js
@@ -1,5 +1,30 @@
 app.controller('AddItemCtrl',
 	function($scope) {
+		var obj = {};
+		obj.date = "2014-03-11";
+		obj.cost = "0";
+		obj.note = "";
+		obj.classname = "測試";
+		$scope.obj = obj;
 		
+		$scope.btnClick = function(value) {
+			var str = $scope.obj.cost;
+			if(value >= 0 && value <= 9){
+				str += value;
+				str = str[0] == "0" ? str.substring(1) : str;
+			}
+			else if(value == "back"){
+				str = str.substring(0, str.length - 1);
+				str = str == "" ? "0" : str;
+			}
+			else if(value == "enter"){
+				document.location.href = "#/itemList";
+			}
+			$scope.obj.cost = str;
+		};
+
+		function checkValue(){
+			return true;
+		}
 	}
 );
