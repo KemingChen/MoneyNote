4d46ce08606c2d558475835ee58599b743af2f64
KemingChen <believe75467@gmail.com>
2014-03-15 13:37:19 +0800

    更新 classList view

--- www/controllers/classListCtrl.js
+++ www/controllers/classListCtrl.js
@@ -1,7 +1,48 @@
-app.controller('ClassListCtrl', function($scope, MNDB) {
+app.controller('ClassListCtrl', function($scope, $ionicActionSheet, $ionicModal, MNDB) {
+	var newClassBtn = {
+		type: 'button-positive',
+		content: '<i>新增</i>',
+		tap: function(){
+			$scope.modal.show();
+		}
+	}
+
+	$scope.title = "";
 	$scope.Classes = [];
+	$scope.rightButtons = [newClassBtn];
 
 	MNDB.selectClasses(onSelectQueryCallback);
+
+	$ionicModal.fromTemplateUrl('class.html', function(modal) {
+    	$scope.modal = modal;
+	}, {
+		scope: $scope,
+		animation: 'slide-in-up'
+	});
+
+	$scope.showSheet = function(iClass){
+		$ionicActionSheet.show({
+			buttons: [
+				{ text: '編　輯' },
+			],
+	      	cancelText: '取　消',
+	      	destructiveText: '刪　除',
+
+			cancel: function() {},
+
+			buttonClicked: function(index) {
+	        	return true;
+	      	},
+
+			destructiveButtonClicked: function() {
+				return true;
+			}
+	    });
+	}
+
+	$scope.closeModal = function (){
+		$scope.modal.hide();
+	}
 
 	function onSelectQueryCallback(array){
 		$scope.Classes = array;
