bf9e568245e560045cf7adbbc356c995ac4077ec
Frank <tftf156@gmail.com>
2014-03-15 14:49:44 +0800

    更新report
    可顯示圖表
    差抓資料及統計

--- www/controllers/reportCtrl.js
+++ www/controllers/reportCtrl.js
@@ -1,96 +1,126 @@
+google.load('visualization', '1', {
+  packages: ['corechart']
+});
+ /*
+google.setOnLoadCallback(function() {
+  angular.bootstrap(document.body, ['MoneyNote']);
+});*/
+
 app.controller('ReportCtrl',
 	function($scope, MNDB, google) {
-	var className;
-		$scope.init = function() {
-			$scope.ControlName = "產生報表";
+		$scope.piechartHistory = [];
+		$scope.piechart = function() {
+			var x = [
+				['className', 'count']
+			];
+			var piechartHistory = [
+				{
+					className: '1月',
+					count: 20
+				},
+				{
+					className: '2月',
+					count: 200
+				},
+				{
+					className: '3月',
+					count: 50
+				},
+				{
+					className: '4月',
+					count: 150
+				}
+			];
+			angular.forEach(piechartHistory,
+				function(record, key) {
+					x.push([
+						record.className,
+						record.count
+					]);
+				}
+			);
+			$scope.piechartHistory = x;
 		};
-		MNDB.selectClasses(onSelectClassQueryCallback);
 		
-		function onSelectClassQueryCallback(array){
-			className = array;
-		}
-		console.log(className);
+		$scope.linechartHistory = [];
+		$scope.linechart = function() {
+			var x = [
+				['month', 'count']
+			];
+			var linechartHistory = [
+				{
+					month: '1月',
+					count: 20
+				},
+				{
+					month: '2月',
+					count: 200
+				},
+				{
+					month: '3月',
+					count: 50
+				},
+				{
+					month: '4月',
+					count: 150
+				}
+			];
+			angular.forEach(linechartHistory,
+				function(record, key) {
+					x.push([
+						record.month,
+						record.count
+					]);
+				}
+			);
+			$scope.linechartHistory = x;
+		};
 	}	
 )
-.directive("drawing", function(){
-  return {
-    restrict: "A",
-    link: function($scope, $element){
-		var ctx = $element[0].getContext('2d');
-		
-		var data = [75,68,32,95,20,51];
-		var className;
-		var colors = ["#7E3817", "#C35817", "#EE9A4D", "#A0C544", "#348017", "#307D7E"];
-		var center = [$element[0].width / 2, $element[0].height / 2];
-		var radius = Math.min($element[0].width, $element[0].height) / 2.5;
-		var lastPosition = 0, total = 0;
-		
-		for(var i in data) { total += data[i]; }
-		
-		for (var i = 0; i < data.length; i++) {
-			ctx.fillStyle = colors[i];
-			ctx.beginPath();
-			ctx.moveTo(center[0],center[1]);
-			ctx.arc(center[0],center[1],radius,lastPosition,lastPosition+(Math.PI*2*(data[i]/total)));
-			ctx.lineTo(center[0],center[1]);
-			ctx.fill();
-			lastPosition += Math.PI*2*(data[i]/total);
-		}
-      /*element.bind('mousedown', function(event){
-        if(event.offsetX!==undefined){
-          lastX = event.offsetX;
-          lastY = event.offsetY;
-        } else {
-          lastX = event.layerX - event.currentTarget.offsetLeft;
-          lastY = event.layerY - event.currentTarget.offsetTop;
-        }
-        
-        // begins new line
-        ctx.beginPath();
-        
-        drawing = true;
-      });
-      element.bind('mousemove', function(event){
-        if(drawing){
-          // get current mouse position
-          if(event.offsetX!==undefined){
-            currentX = event.offsetX;
-            currentY = event.offsetY;
-          } else {
-            currentX = event.layerX - event.currentTarget.offsetLeft;
-            currentY = event.layerY - event.currentTarget.offsetTop;
-          }
-          
-          draw(lastX, lastY, currentX, currentY);
-          
-          // set current coordinates to last one
-          lastX = currentX;
-          lastY = currentY;
-        }
-        
-      });
-      element.bind('mouseup', function(event){
-        // stop drawing
-        drawing = false;
-      });
-        
-      // canvas reset
-      function reset(){
-       //element[0].width = element[0].width; 
-      }
-      
-      function draw(lX, lY, cX, cY){
-        // line from
-        /*ctx.moveTo(lX,lY);
-        // to
-        ctx.lineTo(cX,cY);
-        // color
-        ctx.strokeStyle = "#4bf";
-        // draw it
-        ctx.stroke();
-      }*/
-    }
-  };
-});
+.directive('piechart', 
+	function() {
+		return {
+			restrict: 'E',
+			replace: true,
+			scope: {
+				data: '=data'
+			},
+			template: '<div class="piechart"></div>',
+			link: function(scope, element, attrs) {
+				var chart = new google.visualization.PieChart(element[0]);
+				var options = {title: 'Company Performance'};
+				scope.$watch('data',
+					function(v) {
+						var data = google.visualization.arrayToDataTable(v);
+						chart.draw(data, options);
+					}
+				);
+			}
+		};
+	}
+)
+.directive('linechart', 
+	function() {
+		return {
+			restrict: 'E',
+			replace: true,
+			scope: {
+				data: '=data'
+			},
+			template: '<div class="linechart"></div>',
+			link: function(scope, element, attrs) {
+				var chart = new google.visualization.LineChart(element[0]);
+				var options = {title: 'Company Performance'};
+				scope.$watch('data',
+					function(v) {
+						var data = google.visualization.arrayToDataTable(v);
+						chart.draw(data, options);
+					}
+				);
+			}
+		};
+	}
+);
+
 
 
