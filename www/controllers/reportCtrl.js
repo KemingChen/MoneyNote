641b6bc60f65fe41f2ba7ce4c06f3876feb7f8ee
KemingChen <believe75467@gmail.com>
2014-03-15 15:35:54 +0800

    更新 reportCtrl select bug

--- www/controllers/reportCtrl.js
+++ www/controllers/reportCtrl.js
@@ -6,75 +6,73 @@ google.setOnLoadCallback(function() {
   angular.bootstrap(document.body, ['MoneyNote']);
 });*/
 
-app.controller('ReportCtrl',
-	function($scope, MNDB, google) {
-		
-		$scope.piechartHistory = [];
-		$scope.piechart = function() {
-			var className=[];
-			MNDB.selectClasses(selectClassesCallback);
-			function selectClassesCallback(array)
+app.controller('ReportCtrl', function($scope, MNDB, google) {
+	$scope.piechartHistory = [];
+	$scope.piechart = function() {
+		var className=[];
+		MNDB.selectClasses(selectClassesCallback);
+		function selectClassesCallback(array)
+		{
+			className=array;
+			$scope.apply();
+		}
+		console.log(className);
+		var piechartHistory=[];
+		for (i=0;i<className.length;i++)
+		{
+			piechartHistory[i]=[];
+			piechartHistory[i][0]=className[i].title;
+			piechartHistory[i][1]=0;
+		}
+		console.log(piechartHistory);
+		var test=[
+		['Task', 'Hours per Day'],
+		['Work',     11],
+		['Eat',      2],
+		['Commute',  2],
+		['Watch TV', 2],
+		['Sleep',    7]
+		]
+		$scope.piechartHistory = test;
+
+
+		console.log(test[2][1]);
+	};
+
+	$scope.linechartHistory = [];
+	$scope.linechart = function() {
+		var x = [
+			['month', 'count']
+		];
+		var linechartHistory = [
 			{
-				className=array;
-			}
-			console.log(className);
-			var piechartHistory=[];
-			for (i=0;i<className.length;i++)
+				month: '1月',
+				count: 20
+			},
 			{
-				piechartHistory[i]=[];
-				piechartHistory[i][0]=className[i].title;
-				piechartHistory[i][1]=0;
+				month: '2月',
+				count: 200
+			},
+			{
+				month: '3月',
+				count: 50
+			},
+			{
+				month: '4月',
+				count: 150
 			}
-			console.log(piechartHistory);
-			var test=[
-          ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
-        ]
-			$scope.piechartHistory = test;
-			
-		
-			console.log(test[2][1]);
-		};
-		
-		$scope.linechartHistory = [];
-		$scope.linechart = function() {
-			var x = [
-				['month', 'count']
-			];
-			var linechartHistory = [
-				{
-					month: '1月',
-					count: 20
-				},
-				{
-					month: '2月',
-					count: 200
-				},
-				{
-					month: '3月',
-					count: 50
-				},
-				{
-					month: '4月',
-					count: 150
-				}
-			];
-			angular.forEach(linechartHistory,
-				function(record, key) {
-					x.push([
-						record.month,
-						record.count
-					]);
-				}
-			);
-			$scope.linechartHistory = x;
-		};
-	}	
-)
+		];
+		angular.forEach(linechartHistory,
+			function(record, key) {
+				x.push([
+					record.month,
+					record.count
+				]);
+			}
+		);
+		$scope.linechartHistory = x;
+	};
+})
 .directive('piechart', 
 	function() {
 		return {
